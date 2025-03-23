773983f0b1111b27fcef63016b5e74c091450a6d
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-23 14:18:16 -0500

    Work in progress: Commit to save work on homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,6 +2,13 @@ function HomePage() {
   return (
     <>
       <h1>Home Homie</h1>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1>I am Ken Nguyen</h1>
+          <p>And I'm a Web Developer</p>
+          <button>Shop</button>
+        </div>
+      </div>
     </>
   )
 }
